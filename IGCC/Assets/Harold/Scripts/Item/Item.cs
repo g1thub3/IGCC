dd3a644b9f7eb8693a2cbdfbf89c59e6fb6c3c52
Mitsuteru-0 <134497875+Mitsuteru-0@users.noreply.github.com>
2025-08-23 19:40:02 +0800

    SLot Machine updates

--- IGCC/Assets/Harold/Scripts/Item/Item.cs
+++ IGCC/Assets/Harold/Scripts/Item/Item.cs
@@ -4,7 +4,9 @@ public class Item : MonoBehaviour
 {
     [SerializeField]
     Sprite _sprite;
+    public Sprite Sprite=>_sprite;
 
-
-
+    [SerializeField]
+    string _name;
+    public string Name => _name;
 }
