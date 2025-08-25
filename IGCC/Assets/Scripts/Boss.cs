c0e1b1e7f0ac35d623c9b0f241588463573c702f
Rayyan Putra Zalman <232470k@mymail.nyp.edu.sg>
2025-08-25 14:01:33 +0800

    boss start

---
+++ IGCC/Assets/Scripts/Boss.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Boss : Enemy
+{
+    [SerializeField] BoxCollider _enemyHitbox, _bodyHitbox;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public override void onStateChanged()
+    {
+
+    }
+}
