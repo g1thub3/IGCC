5fd9054ef5cfb1f29baed78fd18477935442209b
Rayyan Putra Zalman <232470k@mymail.nyp.edu.sg>
2025-08-28 14:00:24 +0800

    a

--- IGCC/Assets/Harold/Scripts/Portal.cs
+++ IGCC/Assets/Harold/Scripts/Portal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RoomPortal : MonoBehaviour
@@ -17,7 +18,27 @@ public class RoomPortal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Trigger();
+    }
+
+    public void Trigger()
+    {
         RoomManager.Instance.goToNewRoom(_roomStored);
     }
 
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(RoomPortal))]
+public class PortalEditor : Editor
+{
+    RoomPortal myTarget;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        myTarget = target as RoomPortal;
+        if (GUILayout.Button("Trigger Portal"))
+            myTarget.Trigger();
+    }
+}
+#endif
