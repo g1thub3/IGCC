2064927d71c4c377ac79ecceb7a57f1c5a2ccca9
Mitsuteru-0 <134497875+Mitsuteru-0@users.noreply.github.com>
2025-08-28 11:10:07 +0800

    commit

--- IGCC/Assets/Harold/Scripts/DamageOnContact.cs
+++ IGCC/Assets/Harold/Scripts/DamageOnContact.cs
@@ -6,23 +6,35 @@ public class DamageOnContact : MonoBehaviour
     float _damageVal = 1;
     [SerializeField] bool _killOnHit = true;
     [SerializeField] float _hitForce = 0.0f;
+    Inventory _inventory;
 
     private void Start()
     {
-        
+        _inventory = FindAnyObjectByType<Inventory>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         Health health = collision.gameObject.GetComponent<Health>();
-        if (health != null && _killOnHit)
-        { 
-            health.takeDamage(_damageVal);
-            var controller = collision.GetComponent<MovementController>();
-            collision.GetComponent<MovementController>().AddVelocity((collision.transform.position - transform.position).normalized * _hitForce);
-        } else
+        if (health != null)
         {
-
+            if (_killOnHit)
+            {
+                health.takeDamage(_damageVal);
+                var controller = collision.GetComponent<MovementController>();
+                collision.GetComponent<MovementController>().AddVelocity((collision.transform.position - transform.position).normalized * _hitForce);
+            }
+            else
+            {
+                var control = collision.GetComponent<MovementController>();
+                if (control != null && control.enabled && !health.isInvincible())
+                {
+                    health.takeDamage(0);
+                    _inventory.changeLivesBy(-1);
+                    var controller = collision.GetComponent<MovementController>();
+                    collision.GetComponent<MovementController>().AddVelocity((collision.transform.position - transform.position).normalized * _hitForce);
+                }
+            }
         }
     }
 }
